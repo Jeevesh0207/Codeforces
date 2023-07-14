790625a4548b1791af6964d79e1fbcc48ecaaf29
Jeevesh Rai <132649861+Jeevesh0207@users.noreply.github.com>
2023-07-14 10:44:15 +0530

    Create Sasha and Array Coloring.cpp

---
+++ 800/Sasha and Array Coloring.cpp
@@ -0,0 +1,45 @@
+Questin Link:- (" https://codeforces.com/problemset/problem/1843/A ")
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FastinOut ios::sync_with_stdio(false); cin.tie(0);
+#define TimeStart auto start = chrono::high_resolution_clock::now();
+#define TimeEnd auto stop = chrono::high_resolution_clock::now();
+#define Time  long double duration = chrono::duration_cast<chrono::nanoseconds>(stop - start).count();
+
+
+void Rabbit(){
+
+   ll n;
+   cin>>n;
+   vector<ll>v(n);
+   for(ll i=0;i<n;i++) cin>>v[i];
+   sort(v.begin(),v.end());
+   ll ans=0;
+   for(ll i=0;i<n/2;i++){
+     ans+=(v[n-i-1]-v[i]); 
+   }
+   cout<<ans<<endl;
+}
+
+ 
+int main(){
+    FastinOut;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    TimeStart;
+    ll T=1;
+    cin>>T;
+    while(T--){
+        Rabbit();
+    }
+    TimeEnd; 
+    Time;
+    #ifndef ONLINE_JUDGE
+        std::cerr << "Time taken : " << duration / 1e9 << "s" << std::endl;     
+    #endif
+    return 0;
+}
