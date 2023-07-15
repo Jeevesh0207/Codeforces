fa6f7695135ed67f29adf10036de7946c0e94566
Jeevesh Rai <132649861+Jeevesh0207@users.noreply.github.com>
2023-07-15 19:39:03 +0530

    Create Grasshopper on a Line.cpp

---
+++ 800/Grasshopper on a Line.cpp
@@ -0,0 +1,45 @@
+Question Link :- (" https://codeforces.com/problemset/problem/1837/A ")
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FastinOut ios::sync_with_stdio(false); cin.tie(0);
+#define TimeStart auto start = chrono::high_resolution_clock::now();
+#define TimeEnd auto stop = chrono::high_resolution_clock::now();
+#define Time  long double duration = chrono::duration_cast<chrono::nanoseconds>(stop - start).count();
+
+
+void Rabbit(){
+  ll n,k;
+  cin>>n>>k;
+  if(n%k==0){
+    cout<<"2"<<endl;
+    cout<<n-1<<" "<<1<<endl;
+  }
+  else{
+    cout<<"1"<<endl;
+    cout<<n<<endl;
+  }
+  
+}
+
+ 
+int main(){
+    FastinOut;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    TimeStart;
+    ll T=1;
+    cin>>T;
+    while(T--){
+        Rabbit();
+    }
+    TimeEnd; 
+    Time;
+    #ifndef ONLINE_JUDGE
+        std::cerr << "Time taken : " << duration / 1e9 << "s" << std::endl;     
+    #endif
+    return 0;
+}
