161f28acd047aa6ed05ca21ab2541dcab76f7ac2
Jeevesh Rai <132649861+Jeevesh0207@users.noreply.github.com>
2023-07-15 19:21:10 +0530

    Create Blackboard List.cpp

---
+++ 800/Blackboard List.cpp
@@ -0,0 +1,38 @@
+Question Link :- (" https://codeforces.com/problemset/problem/1838/A ")
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FastinOut ios::sync_with_stdio(false); cin.tie(0);
+#define TimeStart auto start = chrono::high_resolution_clock::now();
+#define TimeEnd auto stop = chrono::high_resolution_clock::now();
+#define Time  long double duration = chrono::duration_cast<chrono::nanoseconds>(stop - start).count();
+
+
+void Rabbit(){
+  ll n,k;
+  cin>>n>>k;
+  cout<<2+(n-2)/k<<endl;
+  
+}
+
+ 
+int main(){
+    FastinOut;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    TimeStart;
+    ll T=1;
+    cin>>T;
+    while(T--){
+        Rabbit();
+    }
+    TimeEnd; 
+    Time;
+    #ifndef ONLINE_JUDGE
+        std::cerr << "Time taken : " << duration / 1e9 << "s" << std::endl;     
+    #endif
+    return 0;
+}
