e182841ffe28aaed1704acb3df7132e9e1f770de
Jeevesh Rai <132649861+Jeevesh0207@users.noreply.github.com>
2023-07-18 09:30:44 +0530

    Create Rudolph and Cut the Rope.cpp

---
+++ 800/Rudolph and Cut the Rope.cpp
@@ -0,0 +1,46 @@
+Question Link :- (" https://codeforces.com/problemset/problem/1846/A ")
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FastinOut ios::sync_with_stdio(false); cin.tie(0);
+#define TimeStart auto start = chrono::high_resolution_clock::now();
+#define TimeEnd auto stop = chrono::high_resolution_clock::now();
+#define Time  long double duration = chrono::duration_cast<chrono::nanoseconds>(stop - start).count();
+
+
+
+
+void Rabbit(){
+  ll n;
+  cin>>n;
+  ll cnt=0;
+  for(ll i=0;i<n;i++){
+    ll x,y;
+    cin>>x>>y;
+    cnt+=(max((ll)0,x-y)>0);
+
+  }
+  cout<<cnt<<endl;
+}
+
+ 
+int main(){
+    FastinOut;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    TimeStart;
+    ll T=1;
+    cin>>T;
+    while(T--){
+        Rabbit();
+    }
+    TimeEnd; 
+    Time;
+    #ifndef ONLINE_JUDGE
+        std::cerr << "Time taken : " << duration / 1e9 << "s" << std::endl;     
+    #endif
+    return 0;
+}
